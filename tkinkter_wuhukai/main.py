dab348cdff697be39cb31b33ebd1989cb805dff9
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-12-18 16:01:01 +0000

    added; scaffold for a desktop app

---
+++ tkinkter_wuhukai/main.py
@@ -0,0 +1,9 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title('Tukar Muka')
+
+window.geometry('600x400')
+label = tkinter.Label(text='Aplikasi menukar rupa paras')
+label.grid(column=0, row=0)
+window.mainloop()

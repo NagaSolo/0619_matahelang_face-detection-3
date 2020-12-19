4faf7e1e2930696a9a6d223a6dc30ea7ff5a625a
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-12-19 16:13:06 +0000

    added, tested; file explorer

--- tkinkter_wuhukai/main.py
+++ tkinkter_wuhukai/main.py
@@ -1,9 +1,32 @@
 import tkinter
 
+def browse_file():
+    from tkinter import filedialog
+    filename = filedialog.askopenfilename(
+        initialdir = "/",
+        title = "Pilih sumber imej",
+        filetypes = (
+            ("jpg files", "*.jpg*"), 
+            ("all files", "*.*")
+        )) 
+       
+    # Change label contents 
+    label_file_explorer.configure(text="Imej untuk sumber dipilih: "+filename) 
+
 window = tkinter.Tk()
 window.title('Tukar Muka')
-
 window.geometry('600x400')
+
 label = tkinter.Label(text='Aplikasi menukar rupa paras')
 label.grid(column=0, row=0)
+
+label_file_explorer = tkinter.Label(text='Sumber imej')
+label.grid(column=0, row=0)
+
+button_explore = tkinter.Button(window, text = "Browse Files", command=browse_file)
+button_explore.grid(column = 1, row = 2)
+
+button_exit = tkinter.Button(window, text = "Exit", command = exit)
+button_exit.grid(column = 1,row = 3)
+
 window.mainloop()

3d77d21df5b6339d8d41f5bed380b479a74dc0f6
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-12-21 15:34:03 +0000

    added; feature to choose driving image

--- tkinkter_wuhukai/main.py
+++ tkinkter_wuhukai/main.py
@@ -1,6 +1,6 @@
 import tkinter
 
-def browse_file():
+def browse_sumber_image():
     from tkinter import filedialog
     filename = filedialog.askopenfilename(
         initialdir = "/",
@@ -11,22 +11,44 @@ def browse_file():
         )) 
        
     # Change label contents 
-    label_sumber_explorer.configure(text="Imej untuk sumber dipilih: "+filename) 
+    label_sumber_explorer.configure(text="Imej untuk sumber dipilih: "+filename)
+
+def browse_driving_image():
+    from tkinter import filedialog
+    filename = filedialog.askopenfilename(
+        initialdir = "/",
+        title = "Pilih sumber imej",
+        filetypes = (
+            ("jpg files", "*.jpg*"), 
+            ("all files", "*.*")
+        )) 
+       
+    # Change label contents 
+    label_driving_explorer.configure(text="Imej untuk sumber dipilih: "+filename)
 
 window = tkinter.Tk()
 window.title('Tukar Muka')
 window.geometry('600x400')
 
 label = tkinter.Label(text='Aplikasi menukar rupa paras')
-label.grid(column=0, row=0)
+label.grid(column=1, row=0)
 
 label_sumber_explorer = tkinter.Label(text='Sumber imej')
-label.grid(column=0, row=0)
+label_sumber_explorer.grid(column=1, row=0)
 
-button_explore = tkinter.Button(window, text = "Browse Files", command=browse_file)
-button_explore.grid(column = 1, row = 2)
+button_sumber_explore = tkinter.Button(window, text = "Browse imej sebagai sumber", command=browse_sumber_image)
+button_sumber_explore.grid(column = 1, row = 2)
 
-button_exit = tkinter.Button(window, text = "Exit", command = exit)
-button_exit.grid(column = 1,row = 3)
+button_sumber_exit = tkinter.Button(window, text = "Exit", command = exit)
+button_sumber_exit.grid(column = 1,row = 3)
+
+label_driving_explorer = tkinter.Label(text='Driving imej')
+label_driving_explorer.grid(column=1, row=5)
+
+button_driving_explore = tkinter.Button(window, text = "Browse imej sebagai driver", command=browse_driving_image)
+button_driving_explore.grid(column=1, row = 6)
+
+button_driving_exit = tkinter.Button(window, text = "Exit", command = exit)
+button_driving_exit.grid(column=1,row = 7)
 
 window.mainloop()

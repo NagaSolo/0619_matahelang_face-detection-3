d742ddd5d38f9211d3925e964d6a3d8647fd44c8
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-12-23 09:47:53 +0000

    updated, clean codebases

--- tkinkter_wuhukai/main.py
+++ tkinkter_wuhukai/main.py
@@ -1,4 +1,5 @@
 import tkinter
+from PIL import ImageTk, Image
 
 def browse_sumber_image():
     from tkinter import filedialog
@@ -10,8 +11,10 @@ def browse_sumber_image():
             ("all files", "*.*")
         )) 
        
-    # Change label contents 
+    # Change label contents
+    # img = ImageTk.PhotoImage(Image.open(filename))
     label_sumber_explorer.configure(text="Imej untuk sumber dipilih: "+filename)
+    # e_sumber.configure(text='Sumber imej: '+filename)
 
 def browse_driving_image():
     from tkinter import filedialog
@@ -24,7 +27,7 @@ def browse_driving_image():
         )) 
        
     # Change label contents 
-    label_driving_explorer.configure(text="Imej untuk sumber dipilih: "+filename)
+    label_driving_explorer.configure(text="Imej untuk driving dipilih: "+filename)
 
 window = tkinter.Tk()
 window.title('Tukar Muka')
@@ -33,22 +36,22 @@ window.geometry('600x400')
 label = tkinter.Label(text='Aplikasi menukar rupa paras')
 label.grid(column=1, row=0)
 
-label_sumber_explorer = tkinter.Label(text='Sumber imej')
-label_sumber_explorer.grid(column=1, row=2)
-
 button_sumber_explore = tkinter.Button(window, text = "Browse imej sebagai sumber", command=browse_sumber_image)
-button_sumber_explore.grid(column = 1, row = 3)
+button_sumber_explore.grid(column=2, row=4)
 
-button_sumber_exit = tkinter.Button(window, text = "Exit", command = exit)
-button_sumber_exit.grid(column = 1,row = 4)
-
-label_driving_explorer = tkinter.Label(text='Driving imej')
-label_driving_explorer.grid(column=1, row=5)
+label_sumber_explorer = tkinter.Label(text='Sumber imej')
+label_sumber_explorer.grid(column=2, row=5)
 
 button_driving_explore = tkinter.Button(window, text = "Browse imej sebagai driver", command=browse_driving_image)
-button_driving_explore.grid(column=1, row = 6)
+button_driving_explore.grid(column=2, row = 7)
 
-button_driving_exit = tkinter.Button(window, text = "Exit", command = exit)
-button_driving_exit.grid(column=1,row = 7)
+label_driving_explorer = tkinter.Label(text='Driving imej')
+label_driving_explorer.grid(column=2, row=8)
+
+e = tkinter.Entry(window, width=25)
+e.grid(column=2, row=9)
+
+button_exit = tkinter.Button(window, text = "Keluar", command = exit)
+button_exit.grid(column=2,row=10)
 
 window.mainloop()

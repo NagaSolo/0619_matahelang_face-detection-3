54d60e9253adaaef4ba5bd6bd89678a95218362a
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-01-05 15:06:00 +0000

    added; function to open source image

--- tkinkter_wuhukai/main_2nd.py
+++ tkinkter_wuhukai/main_2nd.py
@@ -11,7 +11,7 @@ def openfn():
     filename = filedialog.askopenfilename(title='open')
     return filename
 
-def open_img():
+def open_img_sumber():
     x = openfn()
     img = Image.open(x)
     img = img.resize((250, 350), Image.ANTIALIAS)
@@ -22,7 +22,18 @@ def open_img():
     name = Label(root, text=x)
     name.pack()
 
+def open_img_target():
+    x = openfn()
+    img = Image.open(x)
+    img = img.resize((250, 350), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    panel = Label(root, image=img)
+    panel.image = img
+    panel.pack()
+    name = Label(root, text=x)
+    name.pack()
 
-Button(root, text='Pilih Imej', command=open_img).pack()
+Button(root, text='Pilih Imej Sumber', command=open_img_sumber).pack()
 
+Button(root, text='Pilih Imej Target', command=open_img_target).pack()
 root.mainloop()

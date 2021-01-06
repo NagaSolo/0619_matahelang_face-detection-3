097fd9142428a7d95f2b2a9bdd3abcc30f5e103a
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-01-06 09:30:53 +0000

    separated; framing features

--- tkinkter_wuhukai/main_2nd.py
+++ tkinkter_wuhukai/main_2nd.py
@@ -7,6 +7,12 @@ root = Tk()
 root.geometry("550x300+300+150")
 root.resizable(width=True, height=True)
 
+left_frame = Frame(root)
+left_frame.pack(side=LEFT)
+
+right_frame = Frame(root)
+right_frame.pack(side=RIGHT)
+
 def openfn():
     filename = filedialog.askopenfilename(title='open')
     return filename
@@ -16,24 +22,25 @@ def open_img_sumber():
     img = Image.open(x)
     img = img.resize((250, 350), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
-    panel = Label(root, image=img)
+    panel = Label(left_frame, image=img)
     panel.image = img
     panel.pack()
-    name = Label(root, text=x)
-    name.pack()
+    name = Label(left_frame, text=x)
+    name.pack(side=BOTTOM)
+
+Button(left_frame, text='Pilih Imej Sumber', command=open_img_sumber).pack(side=TOP)
 
 def open_img_target():
     x = openfn()
     img = Image.open(x)
     img = img.resize((250, 350), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
-    panel = Label(root, image=img)
+    panel = Label(right_frame, image=img)
     panel.image = img
     panel.pack()
-    name = Label(root, text=x)
-    name.pack()
+    name = Label(right_frame, text=x)
+    name.pack(side=BOTTOM)
 
-Button(root, text='Pilih Imej Sumber', command=open_img_sumber).pack()
+Button(right_frame, text='Pilih Imej Target', command=open_img_target).pack(side=TOP)
 
-Button(root, text='Pilih Imej Target', command=open_img_target).pack()
 root.mainloop()
